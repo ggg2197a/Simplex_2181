8be3928760d20e8ff7052686c8eccb85d144a8a4
Gregory Gonzalez <ggg2197@rit.edu>
2018-09-24 13:29:52 -0400

    E05 - Done in C11

--- C11_TranslationAndScale/AppClass.cpp
+++ C11_TranslationAndScale/AppClass.cpp
@@ -3,8 +3,8 @@ void Application::InitVariables(void)
 {
 	//init the mesh
 	m_pMesh = new MyMesh();
-	//m_pMesh->GenerateCube(1.0f, C_WHITE);
-	m_pMesh->GenerateSphere(1.0f, 5, C_WHITE);
+	m_pMesh->GenerateCube(0.5f, C_BLACK);
+	//m_pMesh->GenerateSphere(1.0f, 5, C_WHITE);
 }
 void Application::Update(void)
 {
@@ -24,16 +24,90 @@ void Application::Display(void)
 
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+
+	//Adds bool
+	static bool goLeft = false;
 	
 	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
 	static float value = 0.0f;
 	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
+	if(!goLeft)
+	{
+		value += 0.01f;
+		if (value > 2)
+		{
+			//Changes direction if a certain distance is reached
+			goLeft = true;
+		}
 
+	}
+	else
+	{
+		value -= 0.01f;
+		if (value < -2)
+		{
+			//Changes direction if a certain distance is reached
+			goLeft = false;
+		}
+	}
 	//matrix4 m4Model = m4Translate * m4Scale;
 	matrix4 m4Model = m4Scale * m4Translate;
 
-	m_pMesh->Render(m4Projection, m4View, m4Model);
+	//m_pMesh->Render(m4Projection, m4View, m4Model);
+	//Generates the centermost line of cubes of the model - the longest continuous line of cubes in the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0.5,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.0,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {2.0,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {2.5,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-0.5,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.0,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-2.0,0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-2.5,0,0,1}));
+	//Generates the first line up from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0.5,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {2.0,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-0.5,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-2.0,0.5,0,1}));
+	//Generates the second line up from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0.5,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.0,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-0.5,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.0,1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,1.0,0,1}));
+	//Generates the third line up from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.0,1.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.0,1.5,0,1}));
+	//Generates the fourth (and last) line up from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,2.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,2.0,0,1}));
+	//Generates the first line down from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0.5,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.0,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {2.5,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-0.5,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.0,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,-0.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-2.5,-0.5,0,1}));
+	//Generates the second line down from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.5,-1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {2.5,-1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.5,-1.0,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-2.5,-1.0,0,1}));
+	//Generates the third (and last) line down from the center of the shape
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0.5,-1.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {1.0,-1.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-0.5,-1.5,0,1}));
+	m_pMesh->Render(m4Projection, m4View, m4Model * matrix4({1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {-1.0,-1.5,0,1}));
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
